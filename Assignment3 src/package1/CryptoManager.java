713d8d5d7777844ff5277861d6668dcfcd4db0de
uumunna21 <uumunna@montgomerycollege.edu>
2024-10-17 00:15:02 -0400

    Add files via upload

---
+++ Assignment3 src/package1/CryptoManager.java
@@ -0,0 +1,114 @@
+package package1;
+
+public class CryptoManager {
+
+    private static final char LOWER_RANGE = ' ';
+    private static final char UPPER_RANGE = '_';
+    private static final int RANGE = UPPER_RANGE - LOWER_RANGE + 1;
+
+    /**
+     * This method determines if a string is within the allowable bounds of ASCII codes 
+     * according to the LOWER_RANGE and UPPER_RANGE characters
+     * @param plainText a string to be encrypted, if it is within the allowable bounds
+     * @return true if all characters are within the allowable bounds, false if any character is outside
+     */
+    public static boolean isStringInBounds(String plainText) {
+        for (int i = 0; i < plainText.length(); i++) {
+            if (plainText.charAt(i) < LOWER_RANGE || plainText.charAt(i) > UPPER_RANGE) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Encrypts a string according to the Caesar Cipher. The integer key specifies an offset
+     * and each character in plainText is replaced by the character "offset" away from it 
+     * @param plainText an uppercase string to be encrypted.
+     * @param key an integer that specifies the offset of each character
+     * @return the encrypted string or error message if the string is out of bounds
+     */
+    public static String caesarEncryption(String plainText, int key) {
+        if (!isStringInBounds(plainText)) {
+            return "The selected string is not in bounds, Try again.";
+        }
+        StringBuilder encryptedText = new StringBuilder();
+        for (int i = 0; i < plainText.length(); i++) {
+            char encryptedChar = (char) ((plainText.charAt(i) + key - LOWER_RANGE) % RANGE + LOWER_RANGE);
+            encryptedText.append(encryptedChar);
+        }
+        return encryptedText.toString();
+    }
+
+    /**
+     * Encrypts a string according to the Bellaso Cipher. Each character in plainText is offset 
+     * according to the ASCII value of the corresponding character in bellasoStr, which is repeated
+     * to correspond to the length of plainText
+     * @param plainText an uppercase string to be encrypted.
+     * @param bellasoStr an uppercase string that specifies the offsets, character by character.
+     * @return the encrypted string
+     */
+    public static String bellasoEncryption(String plainText, String bellasoStr) {
+        if (!isStringInBounds(plainText)) {
+            return "The selected string is not in bounds, Try again.";
+        }
+        StringBuilder encryptedText = new StringBuilder();
+        for (int i = 0; i < plainText.length(); i++) {
+            int key = bellasoStr.charAt(i % bellasoStr.length());
+            char encryptedChar = (char) ((plainText.charAt(i) + key - LOWER_RANGE) % RANGE + LOWER_RANGE);
+            encryptedText.append(encryptedChar);
+        }
+        return encryptedText.toString();
+    }
+
+    /**
+     * Decrypts a string according to the Caesar Cipher. The integer key specifies an offset
+     * and each character in encryptedText is replaced by the character "offset" characters before it.
+     * This is the inverse of the caesarEncryption method.
+     * @param encryptedText an encrypted string to be decrypted.
+     * @param key an integer that specifies the offset of each character
+     * @return the plain text string
+     */
+    public static String caesarDecryption(String encryptedText, int key) {
+        StringBuilder decryptedText = new StringBuilder();
+        for (int i = 0; i < encryptedText.length(); i++) {
+            char decryptedChar = (char) ((encryptedText.charAt(i) - key - LOWER_RANGE + RANGE) % RANGE + LOWER_RANGE);
+            decryptedText.append(decryptedChar);
+        }
+        return decryptedText.toString();
+    }
+
+    /**
+     * Decrypts a string according the Bellaso Cipher. Each character in encryptedText is replaced by
+     * the character corresponding to the character in bellasoStr, which is repeated
+     * to correspond to the length of plainText. This is the inverse of the bellasoEncryption method.
+     * @param encryptedText an uppercase string to be decrypted.
+     * @param bellasoStr an uppercase string that specifies the offsets, character by character.
+     * @return the decrypted string
+     */
+    public static String bellasoDecryption(String encryptedText, String bellasoStr) {
+        StringBuilder decryptedText = new StringBuilder();
+        for (int i = 0; i < encryptedText.length(); i++) {
+            int key = bellasoStr.charAt(i % bellasoStr.length());
+            char decryptedChar = (char) ((encryptedText.charAt(i) - key - LOWER_RANGE + RANGE) % RANGE + LOWER_RANGE);
+            decryptedText.append(decryptedChar);
+        }
+        return decryptedText.toString();
+    }
+
+    public static void main(String[] args) {
+        // Test cases to demonstrate the functionality
+
+        String plainText = "bye";
+        int caesarKey = 3;
+        String bellasoStr = "CAT";
+
+        String caesarEncrypted = caesarEncryption(plainText, caesarKey);
+        System.out.println("Caesar Encrypted: " + caesarEncrypted);
+        System.out.println("Caesar Decrypted: " + caesarDecryption(caesarEncrypted, caesarKey));
+
+        String bellasoEncrypted = bellasoEncryption(plainText, bellasoStr);
+        System.out.println("Bellaso Encrypted: " + bellasoEncrypted);
+        System.out.println("Bellaso Decrypted: " + bellasoDecryption(bellasoEncrypted, bellasoStr));
+    }
+}
---
+++ Assignment3 src/package1/CryptoManagerTestStudent.java
@@ -0,0 +1,65 @@
+package package1;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CryptoManagerTestStudent {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testIsStringInBounds() {
+		assertTrue(CryptoManager.isStringInBounds("BOY"));
+		assertTrue(CryptoManager.isStringInBounds("\"BOY IS TALL\""));
+		assertFalse(CryptoManager.isStringInBounds("boy"));
+		assertFalse(CryptoManager.isStringInBounds("{BOY"));
+		assertFalse(CryptoManager.isStringInBounds("\"THIS TEST THAT SHOULD FAIL BECAUSE { IS OUTSIDE THE RANGE\""));
+	}
+
+	@Test
+	void testCaesarEncryption() {
+		assertEquals("The selected string is not in bounds, Try again.", CryptoManager.caesarEncryption("bye", 3));
+		assertEquals("GHI", CryptoManager.caesarEncryption("DEF", 3));
+		assertEquals("FGH", CryptoManager.caesarEncryption("ABC", 5));
+		assertEquals("KPHQTO", CryptoManager.caesarEncryption("INFORM", 2));
+		assertEquals("XLMW$MW$E$XIWX", CryptoManager.caesarEncryption("THIS IS A TEST", 4));
+	}
+
+	@Test
+	void testBellasoEncryption() {
+		assertEquals("UB_H", CryptoManager.bellasoEncryption("RAKE", "CAT"));
+		assertEquals("TWQ]J", CryptoManager.bellasoEncryption("BRAKE", "REP"));
+		assertEquals("HFPVM", CryptoManager.bellasoEncryption("FAITH", "BEG"));
+	}
+
+	@Test
+	void testCaesarDecryption() {
+		assertEquals("DEF", CryptoManager.caesarDecryption("GHI", 3));
+		assertEquals("HELLO NICE TO MEET YOU", CryptoManager.caesarDecryption("NKRRU&TOIK&ZU&SKKZ&_U[", 6));
+		assertEquals("I NEED TO CODE", CryptoManager.caesarDecryption("N%SJJI%YT%HTIJ", 5));
+	}
+
+	@Test
+	void testBellasoDecryption() {
+		assertEquals("COME DOWN", CryptoManager.bellasoDecryption("DQPF\"GPYQ", "ABC"));
+		assertEquals("HAPPY BIRTHDAY", CryptoManager.bellasoDecryption("KCQS[!EKSWJED[", "CBA"));
+		assertEquals("FAITH", CryptoManager.bellasoDecryption("HFPVM", "BEG"));
+
+	}
+
+	@Test
+	void testMain() {
+		
+	}
+
+}
